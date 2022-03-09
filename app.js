45dcbf063f31a20a09df95b1ba77b584a7a0eee7
THANHDO <dnthanh.19it3@vku.udn.vn>
2022-03-09 11:29:01 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express=require("express");
+const web3 = require("web3");
+const ethjs = require("ethjs");
+const app = express();
+const WebSocket = require('ws');
+
+app.use(express.static("./"));
+
+// app.get("/",function(req,res){
+// 	res.sendFile(__dirname+"/index_ethjs.html");
+// 	res.sendFile(__dirname+"/node_modules/web3/dist/web3.min.js")
+// 	res.sendFile(__dirname+"/node_modules/ethjs/dist/ethjs.js")
+// });
+
+app.listen(8080,function(){console.log("listening on port 8080");});
